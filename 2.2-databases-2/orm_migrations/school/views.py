89e936b1d40bafaf13e1a7ee2ef2abfd4942aec8
Denis Vasilev <pythocoder@gmail.com>
2023-08-13 03:23:57 +0600

    done

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,7 +6,8 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+    students = Student.objects.order_by('name')  # Retrieve data from your model
+    context = {'students': students}  # Create a dictionary with the data
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
