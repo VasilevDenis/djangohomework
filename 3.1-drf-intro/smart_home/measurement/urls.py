68a4126dadc55be40d56e4274483bac3b213d418
Denis Vasilev <pythocoder@gmail.com>
2023-09-24 15:00:11 +0600

    chaos

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
 
+from measurement.models import Sensor, Measurement
+from measurement.serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
+from measurement.views import Sensors, Measurements, UpdateSensor, SensorDetail
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    # Создание датчика.
+    # Получить список датчиков.
+    path('sensors/', Sensors.as_view()),
+    # изменение датчика
+    path('sensors/<int:pk>/', UpdateSensor.as_view()),
+    # Добавить измерение.
+    path('measurements/<sensor>/', Measurements.as_view()),
+    # Получить информацию по конкретному датчику.
+    path('sensor/<int:pk>/', SensorDetail.as_view())
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,30 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from measurement.serializers import MeasurementSerializer, SensorSerializer, SensorDetailSerializer, \
+    MeasurementSensorSerializer
+from measurement.models import Sensor, Measurement
+
+
+class Sensors(generics.ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class UpdateSensor(generics.RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class Measurements(generics.ListCreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSensorSerializer
+
+
+class SensorDetail(generics.ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
